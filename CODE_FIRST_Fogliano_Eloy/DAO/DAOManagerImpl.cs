e39467376d5a31a0c2de782e2f08a87ba479eac5
Eric Ramirez <ericramirezhidalgo@outlook.es>
2024-02-22 17:00:24 +0100

    queries-eric

--- CODE_FIRST_Fogliano_Eloy/DAO/DAOManagerImpl.cs
+++ CODE_FIRST_Fogliano_Eloy/DAO/DAOManagerImpl.cs
@@ -12,6 +12,42 @@ namespace CODE_FIRST_Fogliano_Eloy.DAO
         public DAOManagerImpl(MODEL.ClassicModelsDBContext context)
         {
             this.context = context;
+
+            using (var context = new ShopDbContext())
+            {
+
+                // Query customers from France order by credit limit
+                var customersWithName = context.Customers.Where(c => c.country == "France").OrderBy(c => c.creditLimit).ToList();
+
+                //filtering and sorting
+                var filteredProducts = context.Products.Where(p => p.quantityStock >= 2000 && p.msrp < 100).OrderBy(p => p.productName).ToList();
+
+                //joining entities
+                var customerPayments = context.Customers
+                    .Join(context.Payments,
+                    customer => customer.customerNumber,
+                    payment => payment.customerNumber,
+                    (customer, payment) => new
+                    {
+                        CustomerName = customer.customerFirstName,
+                        PaymentAmount = payment.amount
+
+                    })
+                    .ToList();
+
+                //agregation and grouping
+
+                var employeesPerOffice = context.Employees
+                  .GroupBy(e => e.officeCode)
+                  .Select(o => new
+                  {
+                      OfficeCode = o.Key,
+                      EmployeeCount = o.Count()
+                  })
+                 .ToList();
+
+
+            }
         }
 
 		public void AddCOrders(string file)
