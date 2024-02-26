d23748bafd96fe05b481b12f5f57ad5858bfa3a3
Eric Ramirez <ericramirezhidalgo@outlook.es>
2024-02-26 15:08:16 +0100

    SpecialPriceList

---
+++ CODE_FIRST_Fogliano_Eloy/MODEL/SpecialPriceList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE_FIRST_Fogliano_Eloy.MODEL
+{
+    public class SpecialPriceList
+    {
+        public int CustomerId { get; set; }
+        public string ProductCode { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public virtual Customers Customer { get; set; }
+        [ForeignKey("ProductCode")]
+        public virtual Products Product { get; set; }
+    }
+
+
+}
